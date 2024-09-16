22d4dc71ab27e365a05e09e60d9f535d7164f4a4
Yunseok <watemark1017@gmail.com>
2024-09-16 15:48:38 +0900

    Vercel Web Analytics 연동

--- src/App.jsx
+++ src/App.jsx
@@ -3,10 +3,12 @@ import Game from "./components/game/Game";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import Content from "./components/Content";
+import { Analytics } from "@vercel/analytics/react"
 
 function App() {
   return (
     <>
+      <Analytics />
       <Header />
 
       <Content isMain>
