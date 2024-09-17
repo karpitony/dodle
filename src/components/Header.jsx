185cf8514be7d53554e29056a5a1f4735fd9e583
Yunseok <watemark1017@gmail.com>
2024-09-18 00:37:57 +0900

    구글 SEO 등록을 위한  소유권 증명 + 헤더 이모지 바꾸기

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -6,7 +6,7 @@ export default function Header() {
         boxShadow: "0px 4px 29px 0px rgba(66, 68, 90, 0.14)",
       }}
     >
-      <p className="text-3xl font-bold select-none">🌁 Dodle</p>
+      <p className="text-3xl font-bold select-none">🌃 Dodle</p>
     </header>
   );
 }
